2eb24070e1a8df135a49b28add60a396fa7c549e
flvm-7 <murreddaflavio@gmail.com>
2025-10-21 17:53:05 +0200

    svolgimento esercizio toylexer

--- toylexer/lib/lexer.mll
+++ toylexer/lib/lexer.mll
@@ -8,6 +8,19 @@ let chr = ['a'-'z' 'A'-'Z' '0'-'9']
 let id = letter chr*
 let num = ['0'-'9']|['1'-'9']['0'-'9']*
 
+
+let capital_letter = ['A'-'Z']
+let vowel = ['a' 'e' 'i' 'o' 'u' 'A' 'E' 'I' 'O' 'U']
+let consonants = ['a'-'d' 'A'-'D' 'f'-'h' 'F'-'H' 'l'-'n' 'L'-'N' 'p'-'t' 'P'-'T' 'v'-'z' 'V'-'Z']
+let hex_pref = "0x"|"0X"
+let hex = ['a'-'f' 'A'-'F' '0'-'9']
+
+let a_token = capital_letter chr*
+let b_token = vowel+
+let c_token = consonants* vowel consonants*
+let d_token = (('-')? (num)? '.' (num)?) | ('-' (num)? ('.')? (num)?)
+let e_token = hex_pref hex+
+
 rule read_token =
   parse
   | white { read_token lexbuf }  
@@ -16,6 +29,11 @@ rule read_token =
   | "=" { ASSIGN }
   | "+" { PLUS }
   | ";" { SEQ }  
+  | a_token { ATOK (Lexing.lexeme lexbuf) }
+  | b_token { BTOK (Lexing.lexeme lexbuf) }
+  | c_token { CTOK (Lexing.lexeme lexbuf) }
+  | d_token { DTOK (Lexing.lexeme lexbuf) }
+  | e_token { ETOK (Lexing.lexeme lexbuf) }
   | id { ID (Lexing.lexeme lexbuf) }
-  | num { CONST (Lexing.lexeme lexbuf) }    
+  | num { CONST (Lexing.lexeme lexbuf) }
   | eof { EOF }
--- toylexer/test/test_toylexer.ml
+++ toylexer/test/test_toylexer.ml
@@ -5,3 +5,31 @@ let%test "test_frequencies_1" =
   lexer "x=y; x=x+1" |> frequency 3 = [(ID "x", 3); (ASSIGN, 2); (ID "y", 1)]
 
 (* YOUR TESTS HERE *)
+
+let%test "test_ATOK" =
+  (lexer "G3n3vra" = [ATOK "G3n3vra"; EOF]) && 
+  (lexer "s = Alpha" = [ID "s"; ASSIGN; ATOK "Alpha"; EOF]) &&
+  (lexer "3 F" = [CONST "3"; ATOK "F"; EOF])
+
+let%test "test_BTOK" =
+  (lexer "eiou" = [BTOK "eiou"; EOF]) &&
+  (lexer "a" = [BTOK "a"; EOF]) &&
+  (lexer "io" = [BTOK "io"; EOF])
+
+let%test "test_CTOK" = 
+  (lexer "cel" = [CTOK "cel"; EOF]) && 
+  (lexer "ra" = [CTOK "ra" ; EOF]) && 
+  (lexer "lTTOTTL" = [CTOK "lTTOTTL"; EOF])
+
+let%test "test_DTOK" = 
+  (lexer "pi = 3.14; r = 2" = 
+    [CTOK "pi"; ASSIGN; DTOK"3.14"; SEQ; ID "r"; ASSIGN; CONST "2"; EOF]) &&
+  (lexer "c = -.3; a = -7. + c" = 
+    [ID "c"; ASSIGN; DTOK "-.3"; SEQ; BTOK "a"; ASSIGN; DTOK "-7."; PLUS; ID "c"; EOF]) &&
+  (lexer "(-.3 + 2) + (-3.4)" = 
+    [LPAREN; DTOK"-.3"; PLUS; CONST "2"; RPAREN; PLUS; LPAREN; DTOK "-3.4"; RPAREN ; EOF])
+
+let%test "test_ETOK" =
+  (lexer "0xa1" = [ETOK "0xa1"; EOF]) && 
+  (lexer "0XF3" = [ETOK "0XF3"; EOF]) && 
+  (lexer "0Xff3" = [ETOK "0Xff3"; EOF])
